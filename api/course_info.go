5bd962212ff0f719bdb90b5b5eedbd4afcada3b6
Onojeta Brown <onosbrown.saved@gmail.com>
2021-09-23 20:05:08 +0100

    Update course_info.go

--- api/course_info.go
+++ api/course_info.go
@@ -1,1 +1,17 @@
+package api
 
+//gives information about courses to which students registered and their grades.
+//Also students can provide rating to a course and provide feedback.
+func student_course_details(){}
+
+//Gives list of TAs who are related to each course offered in each term of the year
+//and their permission types.
+func ta_related_to_course(){}
+
+//Gives list of GRADERs who are related to each course offered in each term of
+//the year and their permission types.
+func graders_related_to_course(){}
+
+//Gives list of TEACHERs who are related to each course offered in each term
+//of the year and their permission types.
+func teachers_related_to_course(){}
